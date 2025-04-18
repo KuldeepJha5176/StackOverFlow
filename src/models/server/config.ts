56fe1231199547c963ec79b295dc30ccadfd915f
KuldeepJha5176 <ranacjha@gmail.com>
2025-04-18 20:05:58 +0530

    crated the server model

--- src/models/server/config.ts
+++ src/models/server/config.ts
@@ -1,11 +1,19 @@
 import env from "@/app/env";
 
 import { Client,Avatars, Databases, Storage, Users } from "node-appwrite";
+
 let client = new Client();
 
 client
-    .setEndpoint('https://[HOSTNAME_OR_IP]/v1') // Your API Endpoint
-    .setProject('5df5acd0d48c2') // Your project ID
-    .setKey('919c2d18fb5d4...a2ae413da83346ad2') // Your secret API key
-    .setSelfSigned() // Use only on dev mode with a self-signed SSL cert
-;
+    .setEndpoint(env.appwrite.endpoint) // Your API Endpoint
+    .setProject(env.appwrite.projectId) // Your project ID
+    .setKey(env.appwrite.apiKey) // Your secret API key
+    
+
+const users = new Users(client);
+const databases = new Databases(client); 
+const avatar = new Avatars(client);
+const storage = new Storage(client);
+
+
+export { client, users, databases, avatar, storage };
